2e47c0764b6837e6dc3764b7a2e141c279293dde
Admin <admin@Admins-MacBook-Pro.local>
2021-08-02 09:35:50 +0700

    adding files

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,11 @@
+import '../../App.css'
+
+const SignUp = () => {
+    return (
+        <div>
+            <h1 className='sign-up'>Like & Subcribe</h1>
+        </div>
+    )
+}
+
+export default SignUp
