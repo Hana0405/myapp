ae764096bae7548285e8c3c5e1b7865e5c595ca8
Admin <admin@Admins-MacBook-Pro.local>
2021-08-17 10:43:40 +0700

    adding files

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
+import img2 from './images/img2.jpg'
+import img3 from './images/img-3.jpg'
 
 function Cards() {
   return (
@@ -12,13 +14,13 @@ function Cards() {
             <CardItem
             //   src='images/img-9.jpg'
               //src='test.jpeg'
-              src = 'img/img1.jpeg'
+              src = {img2}
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src={img3}
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -26,19 +28,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src={img2}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src={img3}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src={img2}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
